373efbafc7ef7b33a42961b70ee40c4de5f2ce0f
Matt Hummer <biggreyson@gmail.com>
2023-11-03 15:41:56 -0600

    add hacky preview polling

--- static/js/main.js
+++ static/js/main.js
@@ -83,6 +83,53 @@
 
   }
 
+  class PreviewPoller extends HTMLElement {
+    constructor() {
+      if (document.querySelector("#preview")) {
+        throw new Error("There can only be one instance of the preview available at once.");
+      }
+      super();
+      this.id = "preview";
+      this.interval = null;
+      this.imgEle = null;
+      this.polling = false;
+
+      this.buildElement();
+      this.startInterval();
+    }
+
+    disconnectedCallback() {
+      this.stopInterval();
+    }
+
+    buildElement() {
+      this.imgEle = document.createElement('img');
+
+      this.appendChild(this.imgEle);
+    }
+
+    startInterval() {
+      if (this.polling) return;
+      this.interval = setInterval(this.getNextImage, 200);
+      this.polling = true;
+    }
+
+    stopInterval() {
+      if (!this.polling) return;
+      clearInterval(this.interval);
+      this.interval = null;
+      this.polling = false;
+    }
+
+    getNextImage = async () => {
+      const req = await fetch("/api/preview");
+      const imgSrc = await req.text();
+
+      this.imgEle.src = imgSrc;
+    }
+  }
+
   customElements.define("pseudo-carousel", PseudoCarousel);
-  document.querySelector('main').appendChild(new PseudoCarousel());
+  customElements.define("preview-poller", PreviewPoller);
+  document.querySelector('main').appendChild(new PreviewPoller());
 })();
